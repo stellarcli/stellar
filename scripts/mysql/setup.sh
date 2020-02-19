998cde304869e6f9c833ab18dd4b02289718450c
Andrew Willis <a.willis@redu.co.uk>
2020-02-19 14:23:18 +0000

    fixes mysql setup

--- scripts/mysql/setup.sh
+++ scripts/mysql/setup.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-mysql -e "CREATE DB ${DB_NAME};"
-mysql -e "CREATE USER '${DB_USERNAME}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -e "CREATE DATABASE ${DB_NAME};"
+mysql -e "CREATE USER '${DB_USERNAME}'@'%' IDENTIFIED WITH mysql_native_password BY '${DB_PASSWORD}';"
 mysql -e "GRANT ALL ON ${DB_NAME}.* TO '${DB_USERNAME}'@'%';"
